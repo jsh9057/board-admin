9100ad25ba379c035be2c0bd49933702f7e93b08
jsh9057 <jsh9057@skuniv.ac.kr>
2023-08-18 15:58:40 +0900

    #23 - 비즈니스 로직 테스트, 컨트롤러 테스트 작성
    게시글 목록, 단일 게시글, 게시글 삭제 기능을  만들 예정이므로
    먼저 테스트를 작성 함.

---
+++ src/main/java/com/jsh/boardadminproject/dto/response/ArticleClientResponse.java
@@ -0,0 +1,36 @@
+package com.jsh.boardadminproject.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.jsh.boardadminproject.dto.ArticleDto;
+import java.util.List;
+
+public record ArticleClientResponse(
+        @JsonProperty("_embedded")Embedded embedded,
+        @JsonProperty("page") Page page
+) {
+
+    public static ArticleClientResponse empty(){
+        return new ArticleClientResponse(
+                new Embedded(List.of()),
+                new Page(1,0,1,0)
+        );
+    }
+
+    public static ArticleClientResponse of(List<ArticleDto> articles){
+        return new ArticleClientResponse(
+                new Embedded(articles),
+                new Page(articles.size(), articles.size(), 1, 0)
+        );
+    }
+
+    public List<ArticleDto> articles() { return this.embedded.articles(); }
+
+    public record Embedded(@JsonProperty("articles") List<ArticleDto> articles) {}
+
+    public record Page(
+        int size,
+        long totalElements,
+        int totalPages,
+        int number
+    ) {}
+}

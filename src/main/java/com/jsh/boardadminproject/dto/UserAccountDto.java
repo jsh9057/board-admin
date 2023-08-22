d0862f09ffe452969f37a871e369f6547498e99a
jsh9057 <jsh9057@skuniv.ac.kr>
2023-08-22 17:04:22 +0900

    #27 - 회원 관리 비즈니스 로직 및 컨트롤러 테스트 작성

--- src/main/java/com/jsh/boardadminproject/dto/UserAccountDto.java
+++ src/main/java/com/jsh/boardadminproject/dto/UserAccountDto.java
@@ -8,7 +8,6 @@ import java.util.Set;
 
 public record UserAccountDto(
         String userId,
-        Set<RoleType> roleType,
         String email,
         String nickname,
         String memo,
@@ -17,17 +16,16 @@ public record UserAccountDto(
         LocalDateTime modifiedAt,
         String modifiedBy
 ) {
-    public static UserAccountDto of(String userId, Set<RoleType> roleTypes, String email, String nickname, String memo){
-        return UserAccountDto.of(userId,roleTypes,email,nickname,memo,null,null,null,null);
+    public static UserAccountDto of(String userId, String email, String nickname, String memo){
+        return UserAccountDto.of(userId,email,nickname,memo,null,null,null,null);
     }
-    public static UserAccountDto of(String userId, Set<RoleType> roleTypes, String email, String nickname, String memo, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
-        return new UserAccountDto(userId, roleTypes, email, nickname, memo, createdAt, createdBy, modifiedAt, modifiedBy);
+    public static UserAccountDto of(String userId, String email, String nickname, String memo, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new UserAccountDto(userId, email, nickname, memo, createdAt, createdBy, modifiedAt, modifiedBy);
     }
 
     public static UserAccountDto from(AdminAccount entity){
         return new UserAccountDto(
                 entity.getUserId(),
-                entity.getRoleTypes(),
                 entity.getEmail(),
                 entity.getNickname(),
                 entity.getMemo(),

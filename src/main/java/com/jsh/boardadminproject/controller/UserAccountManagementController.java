0f309b405c7a9585aa70c78847f2d485fd22b61f
jsh9057 <jsh9057@skuniv.ac.kr>
2023-08-30 22:57:53 +0900

    #45 - 회원관리 뷰,컨트롤러 구현

--- src/main/java/com/jsh/boardadminproject/controller/UserAccountManagementController.java
+++ src/main/java/com/jsh/boardadminproject/controller/UserAccountManagementController.java
@@ -1,14 +1,11 @@
 package com.jsh.boardadminproject.controller;
 
+import com.jsh.boardadminproject.dto.response.UserAccountResponse;
 import com.jsh.boardadminproject.service.UserAccountManagementService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/management/user-accounts")
@@ -18,7 +15,25 @@ public class UserAccountManagementController {
     private final UserAccountManagementService userAccountManagementService;
 
     @GetMapping
-    public String userAccounts(Model model){
+    public String userAccounts(Model model) {
+        model.addAttribute(
+                "userAccounts",
+                userAccountManagementService.getUserAccounts().stream().map(UserAccountResponse::from).toList()
+        );
+
         return "management/user-accounts";
+    }
+
+    @ResponseBody
+    @GetMapping("/{userId}")
+    public UserAccountResponse userAccount(@PathVariable String userId) {
+        return UserAccountResponse.from(userAccountManagementService.getUserAccount(userId));
+    }
+
+    @PostMapping("/{userId}")
+    public String deleteUserAccount(@PathVariable String userId) {
+        userAccountManagementService.deleteUserAccount(userId);
+
+        return "redirect:/management/user-accounts";
     }
 }
